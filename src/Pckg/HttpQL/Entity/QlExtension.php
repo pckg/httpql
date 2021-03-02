ceb57aa8831036f89f0ad30f45a4c89fb42f1a60
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-03-02 15:50:25 +0100

    helper

---
+++ src/Pckg/HttpQL/Entity/QlExtension.php
@@ -0,0 +1,20 @@
+<?php namespace Pckg\HttpQL\Entity;
+
+trait QlExtension
+{
+
+    public function applyQlExtension()
+    {
+        $filters = request()->header('X-Pckg-Orm-Filters');
+        if (!$filters) {
+            return $this;
+        }
+        $filters = json_decode($filters, true);
+        if (!isset($filters[0]['k']) || !isset($filters[0]['v'])) {
+            return $this;
+        }
+
+        return $this->where($filters[0]['k'], $filters[0]['v']);
+    }
+
+}
